bb924898c2385bdb7fc9d8951616335d78fde78c
Niels Bom <niels@nielsbom.com>
2026-03-03 13:44:12 +0100

    Add user script for Outlook web mail search

---
+++ scripts/outlook_web_mail.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name        outlook.office.com/mail
+// @namespace   Violentmonkey Scripts
+// @match       https://outlook.office.com/mail/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 2/18/2026, 2:12:23 PM
+// ==/UserScript==
+(async () => {
+  ("use strict");
+
+  const log = (args) => {
+    const PREFIX = "🐵 ";
+    console.log(PREFIX + args);
+  };
+
+  const getElementWithAriaLabelStartingWith = (prefix) => {
+    const element = document.querySelector(`[aria-label^="${prefix}"]`);
+    if (element) return element;
+  };
+
+  const getElementWithAriaLabel = (label) => {
+    const element = document.querySelector(`[aria-label="${label}"]`);
+    if (element) return element;
+  };
+
+  function setInputValueAndSubmit(input, value) {
+    input.focus();
+    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+      window.HTMLInputElement.prototype, 'value'
+    ).set;
+    nativeInputValueSetter.call(input, query);
+    input.dispatchEvent(new Event('input', { bubbles: true }));
+
+    // Press Enter
+    input.dispatchEvent(new KeyboardEvent('keydown', {
+      key: 'Enter', code: 'Enter', keyCode: 13, bubbles: true
+    }));
+  }
+
+  function runOutlookSearch(query) {
+    // Find the search input
+    const searchInput = getElementWithAriaLabelStartingWith("Search for email")
+    if (!searchInput) return false;
+    setInputValueAndSubmit(searchInput, query);
+    return true;
+  }
+
+  // Read ?search= from the URL
+  const params = new URLSearchParams(window.location.search);
+  const query = params.get('search');
+
+  await new Promise(r => setTimeout(r, 1000)); // ms
+
+  log(query)
+
+  if (query) {
+    // Outlook loads dynamically, so wait for the search box to appear
+    const interval = setInterval(() => {
+      if (runOutlookSearch(query)) {
+        clearInterval(interval);
+      }
+    }, 500);
+
+    // Give up after 15 seconds
+    setTimeout(() => clearInterval(interval), 15000);
+  }
+
+})();
